dabf5bc43100ca6c88a57ba58a7603cd4db698a7
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2019-07-05 22:47:17 -0400

    Change environment variable

--- lib/guildship/repo.ex
+++ lib/guildship/repo.ex
@@ -5,6 +5,6 @@ defmodule Guildship.Repo do
     pool_size: 10
 
   def init(_type, config) do
-    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+    {:ok, Keyword.put(config, :url, System.get_env("DB_URL"))}
   end
 end
