77491832294ead92e6767e3173dff130247a4413
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2019-07-06 13:58:34 -0400

    Add open cors

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Guildship.Umbrella.MixProject do
       {:timber, "~> 3.1.2"},
       {:timber_ecto, "~> 2.0"},
       {:timber_phoenix, "~> 1.0"},
-      {:timber_plug, "~> 1.1.0"}
+      {:timber_plug, "~> 1.1.0"},
+      {:corsica, "~> 1.1.2"}
     ]
   end
 
