92e693f04fc2c81f503e3122241997deaa86b8ac
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2019-07-06 23:53:15 -0400

    Cleanup

--- lib/guildship_web/resolvers/guilds.ex
+++ lib/guildship_web/resolvers/guilds.ex
@@ -2,7 +2,7 @@ defmodule GuildshipWeb.Resolvers.Guilds do
   alias Guildship.Guilds
 
   def guilds(_, args, %{context: %{current_user: current_user}}) do
-    with true <- Bodyguard.permit?(Guilds, :get_guilds, current_user) do
+    with :ok <- Bodyguard.permit(Guilds, :get_guilds, current_user) do
       Absinthe.Relay.Connection.from_query(
         Guilds.Guild,
         &Guildship.Repo.all/1,
