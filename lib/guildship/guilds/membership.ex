eb0835430d846d2d1a1757a2afb0fb248bb1d7c3
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2019-07-08 20:47:31 -0400

    Use better error key

--- lib/guildship/guilds/membership.ex
+++ lib/guildship/guilds/membership.ex
@@ -16,7 +16,7 @@ defmodule Guildship.Guilds.Membership do
     membership
     |> cast(params, [:guild_id, :user_id, :role])
     |> validate_required([:guild_id, :user_id])
-    |> unique_constraint(:already_joined,
+    |> unique_constraint(:guild_membership,
       name: "guild_memberships_user_id_guild_id_index",
       message: "Already joined guild."
     )
